8d7c935c6c62931486cec0ad8f30ab9097b94cc9
henntaidesu <498791444@qq.com>
2025-11-13 23:18:47 +0900

    更新pyinstaller.bat和package.json文件版本号至v1.1.16，以保持一致性并反映最新发布版本。

--- pyinstaller.bat
+++ pyinstaller.bat
@@ -2,7 +2,7 @@
 echo Starting PyInstaller packaging process...
 
 :: Set version number (modify this for each release)
-set VERSION=v1.1.15
+set VERSION=v1.1.16
 
 :: Activate conda environment
 call conda activate CS2DB
