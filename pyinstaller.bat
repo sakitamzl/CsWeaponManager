3524a87819e471f46974120da7b79518b01a6b78
henntaidesu <498791444@qq.com>
2025-11-20 01:58:25 +0900

    更新pyinstaller.bat中的版本号至v1.2.0，以反映最新的发布版本。

--- pyinstaller.bat
+++ pyinstaller.bat
@@ -2,7 +2,7 @@
 echo Starting PyInstaller packaging process...
 
 :: Set version number (modify this for each release)
-set VERSION=v1.1.18
+set VERSION=v1.2.0
 
 :: Activate conda environment
 call conda activate CS2DB
